a6539c9c40f41f50bdce30bc2af5f1c9c4b51b48
Isbah Malik <129406085+Isbah-23@users.noreply.github.com>
2024-07-02 23:32:34 +0500

    made Dataset class for random dataset

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,7 @@ import torch
 from PIL import Image
 import torchvision.transforms as transforms
 
-spacy_eng = spacy.load('en_core_web_sm') # english language model for tokenization, named entitiy recognising, POS tagging, parsing, word vector conversions
+spacy_eng = spacy.load('en_core_web_sm') # english language model for tokenization, named entity recognition, POS tagging, parsing, word vector conversions
 
 class Vocabulary:
     def __init__(self, frequency_threshold):
@@ -27,7 +27,7 @@ class Vocabulary:
     
     def build_vocabulary(self, sentence_list):
         frequencies = {}
-        idx = 4 # start after the 3 because special tokens are till 3
+        idx = 4 # start after index 3 because special tokens are till 3
         for sentence in sentence_list:
             for word in self.tokenizer_eng(sentence):
                 if word not in frequencies:
@@ -42,3 +42,31 @@ class Vocabulary:
     def encode(self, text): # encode the text to code using the vocabulary
         tokenized_text = self.tokenizer_eng(text)
         return [self.stoi[token] if token in self.stoi else self.stoi['UNK'] for token in tokenized_text]
+    
+class RandomDataset(torch.utils.data.Dataset):
+    def __init__(self, root_directory, captions_file_name, transform=None, freq_threshold=5):
+        self.root_dir = root_directory
+        self.df = pd.read_csv(os.path.join(root_directory,'images',captions_file_name))
+        self.image_files = self.df["img_name"]
+        self.captions = self.df["img_caption"]
+        self.transform = transform
+        self.vocabulary = Vocabulary(freq_threshold)
+        self.vocabulary.build_vocabulary(self.captions.to_list())
+    
+    def __len__(self):
+        return len(self.df) # returns the input sample size
+    
+    def __getitem__(self, idx):
+        image_file, caption = self.image_files[idx], self.captions[idx]
+        image = Image.open(os.path.join(self.root_dir,'images',image_file)).convert("RGB")
+        if self.transform is not None:
+            image = self.transform(image)
+        encoded_caption = [self.vocabulary.stoi['<SOS>']]
+        encoded_caption.extend(self.vocabulary.encode(caption))
+        encoded_caption.append(self.vocabulary.stoi['<EOS>'])
+        return image, torch.tensor(encoded_caption)
+
+if __name__ == "__main__":
+    rd = RandomDataset('random_dataset','image_captions.csv')
+    print(rd.df.head)
+    print(rd.__getitem__(3))
